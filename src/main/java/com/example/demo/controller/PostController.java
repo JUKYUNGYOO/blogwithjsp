fe36996ec31ed578d125e0748ac1735118dcb06f
jukyung <qhdwndb@gmail.com>
2019-03-17 00:56:05 +0900

    b10

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -6,10 +6,12 @@ import com.example.demo.post.PostDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
@@ -24,8 +26,12 @@ public class PostController {
     public String form(Post post) {
         return "form";
     }
+
     @RequestMapping(value = "/write", method = RequestMethod.POST)
-    public String write(Post post) {
+    public String write(@Valid Post post, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
         post.setRegDate(new Date());
         return "redirect:/post/" + postDao.save(post).getId();
     }
--- src/main/java/com/example/demo/post/Post.java
+++ src/main/java/com/example/demo/post/Post.java
@@ -1,53 +1,37 @@
 package com.example.demo.post;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 public class Post {
     @Id
     @GeneratedValue
     int id;
-    String subject;
 
-    @Column(length = 100000000)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(nullable = false) // 위에 스샷찍고 보니 DB에 널 가능하기에. 이침에 수정하였다!
+     String title;
+
+    @Size(max = 255)
+    String subtitle;
+
+    @NotNull
+    @Size(min = 1, max = 100000000)
+    @Column(length = 100000000, nullable = false) // 여기도 nullable = false 추가!
     String content;
 
     Date regDate;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
 }
